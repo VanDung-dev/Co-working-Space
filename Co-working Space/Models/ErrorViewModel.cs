d6498cd37eb9044cf54382b77ac485ca97c894d3
Nguyễn Lê Văn Dũng <131602164+VanDung-dev@users.noreply.github.com>
2026-07-20 14:29:53 +0700

    feat: thêm cấu trúc cơ bản ASP.NET Core MVC

---
+++ Co-working Space/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Co_working_Space.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
